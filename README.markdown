8e07277fc8be8aacabe6c9e112e77fc5c2c35034
tony10091009 <97461098+tony10091009@users.noreply.github.com>
2022-01-11 00:14:30 +0800

    Add files via upload

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Spline with symbols
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/tony10091009/pen/VwMELOg](https://codepen.io/tony10091009/pen/VwMELOg).
+
+
---
+++ src/script.js
@@ -0,0 +1,51 @@
+Highcharts.chart('container', {
+  chart: {
+    type: 'spline'
+  },
+  title: {
+    text: '2014~2019台灣、日本、泰國入境旅客人數比較圖'
+  },
+  subtitle: {
+    text: '資料來源:UNWTO'
+  },
+  xAxis: {
+    categories: ['2014', '2015', '2016', '2017', '2018', '2019']
+  },
+  yAxis: {
+    title: {
+      text: '入境人數(單位:百萬)'
+    },
+    labels: {
+      formatter: function () {
+        return this.value ;
+      }
+    }
+  },
+  tooltip: {
+    crosshairs: true,
+    shared: true
+  },
+  plotOptions: {
+    spline: {
+      marker: {
+        radius: 4,
+        lineColor: '#666666',
+        lineWidth: 1
+      }
+    }
+  },
+  series: [
+    { name: '台灣',
+     marker: {symbol: 'square'},
+    data: [9.9,10.4,10.7,10.7,11.1,11.9]
+ },
+    {name: '日本',
+    marker: {symbol:'diamond' },
+    data: [13.4,19.7,24,28.7,31.2,31.9]
+  },
+  {
+    name: '泰國',
+    marker: { symbol: 'circle'},
+    data: [24.8,29.9,32.5,35.6,38.2,39.9]
+ }]
+});
